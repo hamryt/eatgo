ad4083d1a525a7d2e4447fe9ae65ff3a03284743
hamryt <rkdl0829@naver.com>
2021-02-08 15:45:49 +0900

    restaurnat, review, menu item

--- eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/EatgoAdminApplication.java
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/EatgoAdminApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EatgoApplication {
+public class EatgoAdminApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(EatgoApplication.class, args);
+		SpringApplication.run(EatgoAdminApplication.class, args);
 	}
 
 }
